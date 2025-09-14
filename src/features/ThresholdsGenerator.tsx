d4e248d1b060efd5357d625e69a8abceedfbb5eb
jubrito <jujubrito@outlook.com>
2025-09-14 20:05:28 -0300

    create main thresholds generator component

---
+++ src/features/ThresholdsGenerator.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import type { ThresholdData } from "../types/types";
+import { UploadCsvBox } from "../components/UploadCsvBox/UploadCsvBox";
+import { Thresholds } from "../components/Thresholds/Thresholds";
+
+export const ThresholdsGenerator = () => {
+  const [thresholdData, setThresholdData] = useState<ThresholdData>({
+    metricValues: [],
+  });
+  return (
+    <>
+      <h1>Dynamic Threshold Generator for Datadog Monitors</h1>
+      <UploadCsvBox setThresholdData={setThresholdData} />
+      <Thresholds
+        endpointName={thresholdData.endpointPath}
+        p95Values={thresholdData.metricValues}
+      />
+    </>
+  );
+};
