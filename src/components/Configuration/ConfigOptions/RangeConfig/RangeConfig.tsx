5f52fe41caea6a5a8c5f51490654d42de5a4be68
jubrito <jujubrito@outlook.com>
2025-09-14 23:09:15 -0300

    extract range field into component

---
+++ src/components/Configuration/ConfigOptions/RangeConfig/RangeConfig.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+type RangeConfigProps = {
+  rangeFieldId: string;
+  defaultValue: number;
+  labels: {
+    field: string;
+    rangeBarLabel?: string;
+    lowLabel?: string;
+    highLabel?: string;
+  };
+};
+
+export const RangeConfig = ({
+  rangeFieldId,
+  defaultValue,
+  labels,
+}: RangeConfigProps) => {
+  const [percentile, setPercentile] = useState(defaultValue);
+  const lowLabel = labels?.lowLabel || "Lower";
+  const highLabel = labels?.highLabel || "Higher";
+  const rangeBarLabel = labels?.rangeBarLabel || "Rigorousness";
+
+  const handlePercentileChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPercentile(Math.round(parseInt(event.target.value)));
+  };
+
+  return (
+    <div className="mt-5">
+      <div className="flex items-center">
+        <label className="text-xl font-bold" htmlFor={rangeFieldId}>
+          {labels.field}
+        </label>
+        <input
+          id={rangeFieldId}
+          className="ml-3 bg-gray-900 pl-2 text-center"
+          type="number"
+          min="1"
+          max="99"
+          value={percentile}
+          onChange={handlePercentileChange}
+        />
+      </div>
+      <>
+        <div className="w-max">
+          <p className="inline text-sm font-italic">{lowLabel}</p>
+          <input
+            type="range"
+            className="w-23 h-auto inline mx-3 align-middle"
+            min="1"
+            max="99"
+            value={percentile}
+            onChange={handlePercentileChange}
+          />
+
+          <p className="inline text-sm">{highLabel}</p>
+        </div>
+        <p className="text-sm italic">{rangeBarLabel}</p>
+      </>
+    </div>
+  );
+};
