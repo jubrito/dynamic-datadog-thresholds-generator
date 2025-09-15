6471db6591e611a88d7d0467d831e6c3e80fe2bb
jubrito <jujubrito@outlook.com>
2025-09-14 21:12:09 -0300

    add configuration component template

--- src/components/Configuration/Configuration.tsx
+++ src/components/Configuration/Configuration.tsx
@@ -1,7 +1,11 @@
 export const Configuration = () => {
   return (
-    <section className="w-full">
-      <p>OI</p>
+    <section className="w-full h-full align-left text-left">
+      <h2 className="text-2xl">Generator configuration</h2>
+      <h3 className="text-lg my-5">How rigorous do you want to be?</h3>
+      <div className="p-10 w-full h-full border-1 border-white rounded-lg">
+        <p>test</p>
+      </div>
     </section>
   );
 };
