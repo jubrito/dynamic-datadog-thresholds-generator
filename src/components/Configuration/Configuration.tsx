641a9d5759dbec5e33a895242bfd0e4c4c217ce6
jubrito <jujubrito@outlook.com>
2025-09-14 23:43:40 -0300

    improve config description and style

--- src/components/Configuration/Configuration.tsx
+++ src/components/Configuration/Configuration.tsx
@@ -2,29 +2,31 @@ import { ConfigOptions } from "./ConfigOptions/ConfigOptions";
 
 export const Configuration = () => {
   return (
-    <section className="w-full h-max align-left text-left">
-      <h2 className="text-2xl">Generator configuration</h2>
-      <h3 className="text-lg mt-1 mb-5">How rigorous do you want to be?</h3>
+    <section className="flex flex-col items-end h-max">
+      <div className="align-left">
+        <h2 className="font-bold text-2xl w-full bg-[#2a2c3e] pl-8 py-3 text-left rounded-t-lg">
+          Generator configuration
+        </h2>
 
-      <div className="p-8 w-full h-full border-1 border-white rounded-lg max-w-[505px]">
-        <div className="flex justify-between">
-          <ConfigOptions
-            thresholdType="Critical"
-            defaultPercentile={99}
-            defaultFactor={95}
-          />
-          <ConfigOptions
-            thresholdType="Warning"
-            defaultPercentile={95}
-            defaultFactor={80}
-          />
+        <div className="p-8 w-full h-full bg-[#11131f] max-w-[505px] rounded-b-lg">
+          <div className="flex justify-between">
+            <ConfigOptions
+              thresholdType="Critical"
+              defaultPercentile={99}
+              defaultFactor={95}
+            />
+            <ConfigOptions
+              thresholdType="Warning"
+              defaultPercentile={95}
+              defaultFactor={80}
+            />
+          </div>
+          <p className="mt-8 text-sm">
+            Tip: High rigour factors and percentiles will generate stricter
+            results and lead to less noisy alerts by increasing rigorousness and
+            focusing on the slowest requests instead of the typical ones.
+          </p>
         </div>
-        <p className="mt-8 text-sm">
-          Tip: The higher the percentile and the factor chosen, the stricter the
-          result will be. If you are more strict, you will generate thresholds
-          that will lead to less noisy alerts and will focus on slowest
-          requests.
-        </p>
       </div>
     </section>
   );
