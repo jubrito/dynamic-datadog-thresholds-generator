af222f70de0250fb4c35cc972c56b3243f4f2c81
jubrito <jujubrito@outlook.com>
2025-09-14 23:43:47 -0300

    increase main gap

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ThresholdsGenerator } from "./features/ThresholdsGenerator";
 
 function App() {
   return (
-    <main className="grid grid-cols-2 grid-rows-1 gap-10">
+    <main className="grid grid-cols-2 grid-rows-1 gap-15">
       <Configuration />
       <ThresholdsGenerator />
     </main>
