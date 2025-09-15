a04e684c43c33676948b894da8965168d3c7848d
jubrito <jujubrito@outlook.com>
2025-09-14 22:25:40 -0300

    update configuration layout

--- src/components/Configuration/Configuration.tsx
+++ src/components/Configuration/Configuration.tsx
@@ -2,13 +2,13 @@ import { ConfigOptions } from "./ConfigOptions/ConfigOptions";
 
 export const Configuration = () => {
   return (
-    <section className="w-full h-full align-left text-left">
+    <section className="size-full align-left text-left">
       <h2 className="text-2xl">Generator configuration</h2>
       <h3 className="text-lg my-5">How rigorous do you want to be?</h3>
 
-      <div className="flex p-8 w-full h-full border-1 border-white rounded-lg">
-        <ConfigOptions thresholdOption="Critical" />
-        <ConfigOptions thresholdOption="Warning" />
+      <div className="flex justify-between p-8 h-full border-1 border-white rounded-lg">
+        <ConfigOptions thresholdType="Critical" defaultPercentile={99} />
+        <ConfigOptions thresholdType="Warning" defaultPercentile={95} />
       </div>
     </section>
   );
