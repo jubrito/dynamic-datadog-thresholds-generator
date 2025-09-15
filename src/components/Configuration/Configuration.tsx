15fba8ffc245f56cd1d385a39c07a0bec6dfb650
jubrito <jujubrito@outlook.com>
2025-09-14 21:58:49 -0300

    use config options component

--- src/components/Configuration/Configuration.tsx
+++ src/components/Configuration/Configuration.tsx
@@ -1,10 +1,14 @@
+import { ConfigOptions } from "./ConfigOptions/ConfigOptions";
+
 export const Configuration = () => {
   return (
     <section className="w-full h-full align-left text-left">
       <h2 className="text-2xl">Generator configuration</h2>
       <h3 className="text-lg my-5">How rigorous do you want to be?</h3>
-      <div className="p-10 w-full h-full border-1 border-white rounded-lg">
-        <p>test</p>
+
+      <div className="flex p-8 w-full h-full border-1 border-white rounded-lg">
+        <ConfigOptions thresholdOption="Critical" />
+        <ConfigOptions thresholdOption="Warning" />
       </div>
     </section>
   );
