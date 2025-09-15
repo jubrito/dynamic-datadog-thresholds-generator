da56d91dc7d458db4e968e93cd3c52381601f436
jubrito <jujubrito@outlook.com>
2025-09-14 22:26:52 -0300

    update config description margin

--- src/components/Configuration/Configuration.tsx
+++ src/components/Configuration/Configuration.tsx
@@ -4,7 +4,7 @@ export const Configuration = () => {
   return (
     <section className="size-full align-left text-left">
       <h2 className="text-2xl">Generator configuration</h2>
-      <h3 className="text-lg my-5">How rigorous do you want to be?</h3>
+      <h3 className="text-lg mt-1 mb-5">How rigorous do you want to be?</h3>
 
       <div className="flex justify-between p-8 h-full border-1 border-white rounded-lg">
         <ConfigOptions thresholdType="Critical" defaultPercentile={99} />
