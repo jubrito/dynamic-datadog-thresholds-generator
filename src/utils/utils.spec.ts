b193f5880970ee2147b0693d4d3528230399c947
jubrito <jujubrito@outlook.com>
2025-09-16 00:10:44 -0300

    test: should return maximum

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -3,9 +3,15 @@ import { getStatistics } from "./utils";
 describe("utils", () => {
   describe("getStatistics", () => {
     const sorted = [1, 2, 3, 4, 5, 6];
+
     it("should return minimum", () => {
       const { minimum } = getStatistics(sorted);
       expect(minimum).toBe(sorted[0]);
+    });
+
+    it("should return maximum", () => {
+      const { maximum } = getStatistics(sorted);
+      expect(maximum).toBe(sorted[sorted.length - 1]);
     });
   });
 });
