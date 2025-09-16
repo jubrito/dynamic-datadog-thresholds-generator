b6b87a1083204a6bfa7002b8eafc9b7f884f1346
jubrito <jujubrito@outlook.com>
2025-09-16 19:28:49 -0300

    hide/show stats

--- src/components/ThresholdResults/Statistics/Statistics.tsx
+++ src/components/ThresholdResults/Statistics/Statistics.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { EndpointStats } from "../../../types/types";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 type EndpointStatisticsProps = {
   endpointStats: EndpointStats;
@@ -7,30 +9,43 @@ type EndpointStatisticsProps = {
 export const EndpointStatistics = ({
   endpointStats,
 }: EndpointStatisticsProps) => {
+  const [showStats, setShowStats] = useState(false);
   const insufficientData = endpointStats.numberOfElements < 4;
 
   return (
     <>
-      <p>
-        <strong>Sorted percentiles:</strong> [{endpointStats.sorted}]
-      </p>
-      <p>
-        <strong>Number of values analyzed:</strong>{" "}
-        {endpointStats.numberOfElements}
-      </p>
-      <p>
-        <strong>Minimum:</strong> {endpointStats.minimum}
-        <strong>Maximum:</strong> {endpointStats.maximum}
-        <strong>Average:</strong> {endpointStats.average}
-        <strong>Median:</strong> {endpointStats.median}
-      </p>
-      {insufficientData && (
-        <p>
-          <i>
-            <strong>Note:</strong> Not enough data: enter more than four values
-            for a more accurate result.
-          </i>
-        </p>
+      <div>
+        <div className="flex">
+          <button onClick={() => setShowStats((prevValue) => !prevValue)}>
+            <KeyboardArrowDownIcon />
+            Show endpoint insights
+          </button>
+        </div>
+      </div>
+      {showStats && (
+        <div>
+          <p>
+            <strong>Sorted percentiles:</strong> [{endpointStats.sorted}]
+          </p>
+          <p>
+            <strong>Number of values analyzed:</strong>{" "}
+            {endpointStats.numberOfElements}
+          </p>
+          <p>
+            <strong>Minimum:</strong> {endpointStats.minimum}
+            <strong>Maximum:</strong> {endpointStats.maximum}
+            <strong>Average:</strong> {endpointStats.average}
+            <strong>Median:</strong> {endpointStats.median}
+          </p>
+          {insufficientData && (
+            <p>
+              <i>
+                <strong>Note:</strong> Not enough data: enter more than four
+                values for a more accurate result.
+              </i>
+            </p>
+          )}
+        </div>
       )}
     </>
   );
