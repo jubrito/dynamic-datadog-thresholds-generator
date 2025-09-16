a5ae67dfaf5237fb8090a5b3fadf35e573cd058e
jubrito <jujubrito@outlook.com>
2025-09-16 18:38:28 -0300

    render statistics component extracted

--- src/components/ThresholdResults/ThresholdResults.tsx
+++ src/components/ThresholdResults/ThresholdResults.tsx
@@ -1,4 +1,5 @@
 import type { EndpointStats, Percentile } from "../../types/types";
+import { EndpointStatistics } from "./Statistics/Statistics";
 
 type ThresholdProps = {
   endpointStats: EndpointStats;
@@ -11,41 +12,19 @@ export const ThresholdResults = ({
   warningThreshold,
   criticalThreshold,
 }: ThresholdProps) => {
-  const insufficientData = endpointStats.numberOfElements < 4;
-
   return (
     <>
       <div>
         <div className="flex gap-5">
-          <h3 className="font-bold text-xl my-5 p-2 bg-pink-500 w-max">
+          <h3 className="font-bold text-xl my-5 p-2 bg-pink-500 w-1/2">
             Critical Threshold {criticalThreshold}
           </h3>
-          <h3 className="font-bold my-5 text-xl p-2 bg-[#F66500] w-max">
+          <h3 className="font-bold my-5 text-xl p-2 bg-[#F66500] w-1/2">
             Warning Threshold {warningThreshold}
           </h3>
         </div>
-        <p>
-          <strong>Sorted percentiles:</strong> [{endpointStats.sorted}]
-        </p>
-        <p>
-          <strong>Number of values analyzed:</strong>{" "}
-          {endpointStats.numberOfElements}
-        </p>
-        <p>
-          <strong>Minimum:</strong> {endpointStats.minimum}
-          <strong>Maximum:</strong> {endpointStats.maximum}
-          <strong>Average:</strong> {endpointStats.average}
-          <strong>Median:</strong> {endpointStats.median}
-        </p>
-        {insufficientData && (
-          <p>
-            <i>
-              <strong>Note:</strong> Not enough data: enter more than four
-              values for a more accurate result.
-            </i>
-          </p>
-        )}
       </div>
+      <EndpointStatistics endpointStats={endpointStats} />
     </>
   );
 };
