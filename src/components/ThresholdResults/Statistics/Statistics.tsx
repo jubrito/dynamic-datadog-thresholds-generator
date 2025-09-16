01af6a54e3aba0fd330d8b3dc96980eaff774270
jubrito <jujubrito@outlook.com>
2025-09-16 19:32:29 -0300

    add cursor pointer

--- src/components/ThresholdResults/Statistics/Statistics.tsx
+++ src/components/ThresholdResults/Statistics/Statistics.tsx
@@ -19,6 +19,7 @@ export const EndpointStatistics = ({
           <button
             onClick={() => setShowStats((prevValue) => !prevValue)}
             aria-controls={statsSectionId}
+            className="bg-white text-black p-2 cursor-pointer"
           >
             <KeyboardArrowDownIcon />
             Show endpoint insights
