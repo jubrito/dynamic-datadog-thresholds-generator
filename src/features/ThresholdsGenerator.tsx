e54ae84c6242a4630697ef87108497ec95c57cad
jubrito <jujubrito@outlook.com>
2025-09-16 22:16:38 -0300

    remove description of page from generator

--- src/features/ThresholdsGenerator.tsx
+++ src/features/ThresholdsGenerator.tsx
@@ -16,21 +16,7 @@ export const ThresholdsGenerator = ({
   });
 
   return (
-    <section className="flex flex-col gap-6 text-left">
-      <h1 className="font-bold text-3xl uppercase">
-        Dynamic Threshold Generator
-      </h1>
-      <h3 className="text-lg">
-        <span id="extract-and-upload-csv-description">
-          Extract a Datadog endpoint metrics csv file using any percentile
-          aggregation and upload it to generate warning and critical thresholds
-          suggestions.
-        </span>
-        <span className="pl-1" id="configure-generator-description">
-          Decide how rigorous you want to be by adjusting the configuration
-          values at any time.
-        </span>
-      </h3>
+    <section className="flex flex-col gap-6 text-left bg-[#0f131e] p-6 rounded-xl">
       <Documentation />
       <UploadCsvBox setThresholdData={setThresholdData} />
       <Thresholds
