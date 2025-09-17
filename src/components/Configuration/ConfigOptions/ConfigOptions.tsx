ac26789dbcb30a5a1398d1b64dc85678661a9d9a
jubrito <jujubrito@outlook.com>
2025-09-17 01:14:14 -0300

    pass min max as props

--- src/components/Configuration/ConfigOptions/ConfigOptions.tsx
+++ src/components/Configuration/ConfigOptions/ConfigOptions.tsx
@@ -30,12 +30,20 @@ export const ConfigOptions = ({
         }}
         rangeFieldId={`${thresholdType}-base-percentile`}
         updateConfigProperty={(percentile) => updateConfig({ percentile })}
+        limits={{
+          min: 1,
+          max: 99,
+        }}
       />
       <RangeConfig
         defaultValue={defaultFactor}
         labels={{ field: "Rigorour factor" }}
         rangeFieldId={`${thresholdType}-rigour-factor`}
         updateConfigProperty={(factor) => updateConfig({ factor })}
+        limits={{
+          min: 0,
+          max: 150,
+        }}
       />
     </div>
   );
