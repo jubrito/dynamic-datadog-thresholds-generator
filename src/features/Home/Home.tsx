9b01005e85ec75ad964b5af3423193154abd695f
jubrito <jujubrito@outlook.com>
2025-09-16 22:15:04 -0300

    change grid order

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -16,7 +16,23 @@ export const Home = () => {
   });
 
   return (
-    <main className="grid xl:grid-cols-2 grid-cols-1 grid-rows-1 gap-10">
+    <main className="grid xl:grid-cols-2 grid-cols-2 grid-rows-1 gap-10">
+      <div className="col-span-2 max-h-min text-left">
+        <h1 className="font-bold text-3xl uppercase">
+          Dynamic Threshold Generator
+        </h1>
+        <h3 className="text-lg mt-3">
+          <span id="extract-and-upload-csv-description">
+            Extract a Datadog endpoint metrics csv file using any percentile
+            aggregation and upload it to generate warning and critical
+            thresholds suggestions.
+          </span>
+          <span className="pl-1" id="configure-generator-description">
+            Decide how rigorous you want to be by adjusting the configuration
+            values at any time.
+          </span>
+        </h3>
+      </div>
       <ThresholdsGenerator thresholdsConfig={thresholdsConfig} />
       <Configuration
         thresholdsConfig={thresholdsConfig}
