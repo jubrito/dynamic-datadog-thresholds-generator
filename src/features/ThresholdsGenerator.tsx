8ff432c6f8ab64895d7234b057f265eecdb59993
jubrito <jujubrito@outlook.com>
2025-09-16 23:42:24 -0300

    receive threshold data

--- src/features/ThresholdsGenerator.tsx
+++ src/features/ThresholdsGenerator.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import type { ThresholdData, ThresholdsConfig } from "../types/types";
 import { UploadCsvBox } from "../components/UploadCsvBox/UploadCsvBox";
 import { Thresholds } from "../components/Thresholds/Thresholds";
@@ -6,24 +5,26 @@ import { Documentation } from "../components/Documentation/Documentation";
 
 type ThresholdsGeneratorProps = {
   thresholdsConfig: ThresholdsConfig;
+  thresholdData: ThresholdData;
+  updateThresholdData: React.Dispatch<React.SetStateAction<ThresholdData>>;
 };
 
 export const ThresholdsGenerator = ({
   thresholdsConfig,
+  thresholdData,
+  updateThresholdData,
 }: ThresholdsGeneratorProps) => {
-  const [thresholdData, setThresholdData] = useState<ThresholdData>({
-    metricValues: [],
-  });
-
   return (
-    <section className="flex flex-col gap-4 text-left bg-[#0f131e] p-5 rounded-xl">
-      <Documentation />
-      <UploadCsvBox setThresholdData={setThresholdData} />
-      <Thresholds
-        endpointName={thresholdData.endpointPath}
-        percentileValues={thresholdData.metricValues}
-        thresholdsConfig={thresholdsConfig}
-      />
-    </section>
+    <>
+      <section className="flex flex-col gap-4 text-left bg-[#0f131e] p-5 rounded-xl">
+        <Documentation />
+        <UploadCsvBox setThresholdData={updateThresholdData} />
+        <Thresholds
+          endpointName={thresholdData.endpointPath}
+          percentileValues={thresholdData.metricValues}
+          thresholdsConfig={thresholdsConfig}
+        />
+      </section>
+    </>
   );
 };
