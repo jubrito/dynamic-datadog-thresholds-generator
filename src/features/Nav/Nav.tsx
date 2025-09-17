29945e214a05cf48af3981433527e2bd971b48c3
jubrito <jujubrito@outlook.com>
2025-09-17 20:29:48 -0300

    create nav component

---
+++ src/features/Nav/Nav.tsx
@@ -0,0 +1,13 @@
+export const Nav = () => {
+  return (
+    <nav aria-labelledby="mainmenulabel">
+      <h2 id="mainmenulabel" className="sr-only">
+        Main Menu
+      </h2>
+      <ul>
+        <li>Generator</li>
+        <li>Documentation</li>
+      </ul>
+    </nav>
+  );
+};
