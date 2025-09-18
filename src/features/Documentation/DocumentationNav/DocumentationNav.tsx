99260fd9b0c972c84937d7384841e939c3a3cc60
jubrito <jujubrito@outlook.com>
2025-09-17 23:13:24 -0300

    create DocumentationNav

---
+++ src/features/Documentation/DocumentationNav/DocumentationNav.tsx
@@ -0,0 +1,13 @@
+export const DocumentationNav = () => {
+  return (
+    <ul>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+    </ul>
+  );
+};
