92c34b41be0ffaac0dcb30e0f42c19863d43e8b3
jubrito <jujubrito@outlook.com>
2025-09-18 01:32:53 -0300

    style doc

--- src/features/Documentation/Documentation.tsx
+++ src/features/Documentation/Documentation.tsx
@@ -1,11 +1,14 @@
+import { mainGridStyle } from "../../utils/styles";
 import { DocumentationContent } from "./DocumentationContent/DocumentationContent";
 import { DocumentationNav } from "./DocumentationNav/DocumentationNav";
 
 export const Documentation = () => {
   return (
-    <section className="w-max max-w-350 grid xl:grid-cols-2 grid-cols-2 grid-rows-1 gap-7">
+    <section className={`${mainGridStyle} w-full`}>
       <DocumentationNav />
-      <DocumentationContent />
+      <div className="col-start-2 col-end-4">
+        <DocumentationContent />
+      </div>
     </section>
   );
 };
