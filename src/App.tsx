7d188ab7b46e59bd2b5cc423e5710f04688f12c6
jubrito <jujubrito@outlook.com>
2025-09-17 21:50:47 -0300

    add routes and elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { Home } from "./features/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Nav } from "./features/Nav/Nav";
+import { Generator } from "./features/Generator/Generator";
+import { Documentation } from "./features/Documentation/Documentation";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <main className="grid grid-col-1 grid-row-2 gap-2">
+        <Nav />
+        <div className="my-0 mx-auto max-w-350">
+          <Routes>
+            <Route path="/" element={<Generator />} />
+            <Route path="/documentation" element={<Documentation />} />
+          </Routes>
+        </div>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
