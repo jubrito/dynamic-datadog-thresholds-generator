22438fa45c5a7b00e1fcabd61cfa7f3b2be6149d
jubrito <jujubrito@outlook.com>
2025-09-26 17:51:35 -0300

    scroll to results after they are generated

--- src/components/GeneratorResults/GeneratorResults.tsx
+++ src/components/GeneratorResults/GeneratorResults.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { ThresholdData, ThresholdsConfig } from "../../types/types";
 import { getSortedAscending, getStatistics } from "../../utils/utils";
 import { EndpointStatistics } from "../ThresholdResults/Statistics/Statistics";
@@ -19,9 +20,21 @@ export const GeneratorResults = ({
     ? "bg-[#0f131e] max-h-113 mt-5"
     : "bg-transparent max-h-0 mt-0";
 
+  const resultsRef = useRef<HTMLDivElement>(null);
+
+  const scrollToResults = () => {
+    resultsRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    if (!showInsights) return;
+    scrollToResults();
+  }, [showInsights]);
+
   return (
     <div
       className={`${showMainStyleTransition} xl:col-span-2 text-left border-0 transition-all duration-1000 ease-in-out ease-out overflow-hidden h-min p-5 rounded-xl`}
+      ref={resultsRef}
     >
       <Thresholds
         endpointName={thresholdData.endpointPath}
