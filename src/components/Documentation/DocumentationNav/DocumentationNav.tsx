8ec4d141edc27d3f4648f7c3915f5f0974d67e24
jubrito <jujubrito@outlook.com>
2025-10-01 20:32:16 -0300

    increase border

--- src/components/Documentation/DocumentationNav/DocumentationNav.tsx
+++ src/components/Documentation/DocumentationNav/DocumentationNav.tsx
@@ -14,7 +14,7 @@ export const DocumentationNav = ({
   documentationSectionOpen,
 }: DocumentationNavProps) => {
   return (
-    <div className="bg-black border-l-20 lg:border-l-0 lg:border-r-20 border-[#030712] h-dvh h-auto p-9">
+    <div className="bg-black border-l-20 lg:border-l-0 lg:border-r-35 border-[#030712] h-dvh h-auto p-9">
       <ul className="flex flex-col gap-8 mt-3 cursor-pointer text-right">
         <li>
           <DocNavButton
