ceda326acb0bc84cd1c5b77064cf34a7eaff3f8d
jubrito <jujubrito@outlook.com>
2025-10-01 23:38:32 -0300

    test: should render description

--- src/components/Introduction/Introduction.spec.tsx
+++ src/components/Introduction/Introduction.spec.tsx
@@ -6,4 +6,17 @@ describe("BorderBox", () => {
     render(<Introduction />);
     expect(screen.getByText("Dynamic Threshold Generator")).toBeInTheDocument();
   });
+  it("should render description", () => {
+    render(<Introduction />);
+    expect(
+      screen.getByText(
+        /Extract a Datadog endpoint metrics csv file using any percentile aggregation and upload it to generate warning and critical thresholds suggestions./
+      )
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        /Decide how rigorous you want to be by adjusting the configuration values at any time./
+      )
+    ).toBeInTheDocument();
+  });
 });
