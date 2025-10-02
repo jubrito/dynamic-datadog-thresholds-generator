d7e1a3eb3a632dc1f60f9469916d62d9664f949e
jubrito <jujubrito@outlook.com>
2025-10-01 23:32:55 -0300

    test: should apply custom styles

--- src/components/BorderBox/BorderBox.spec.tsx
+++ src/components/BorderBox/BorderBox.spec.tsx
@@ -2,8 +2,14 @@ import { render } from "@testing-library/react";
 import { BorderBox } from "./BorderBox";
 
 describe("BorderBox", () => {
-  it("renders children correctly", () => {
+  it("should render children correctly", () => {
     const { getByText } = render(<BorderBox>Content</BorderBox>);
     expect(getByText("Content")).toBeInTheDocument();
+  });
+  it("should apply custom styles", () => {
+    const { container } = render(
+      <BorderBox style="custom-style">Content</BorderBox>
+    );
+    expect(container.firstChild).toHaveClass("custom-style");
   });
 });
