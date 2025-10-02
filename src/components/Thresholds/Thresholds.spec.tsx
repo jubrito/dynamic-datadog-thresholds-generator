1346c899ae3dda298663e85141a4868006557950
jubrito <jujubrito@outlook.com>
2025-10-01 22:05:43 -0300

    test: should render endpoint name and thresholds when data is present

--- src/components/Thresholds/Thresholds.spec.tsx
+++ src/components/Thresholds/Thresholds.spec.tsx
@@ -20,7 +20,7 @@ jest.mock("../../utils/thresholds", () => ({
 }));
 
 describe("Thresholds", () => {
-  it.only("should not render results when there are no endpointName or percentile value", () => {
+  it("should not render results when there are no endpointName or percentile value", () => {
     render(
       <Thresholds
         sortedPercentileValues={[]}
@@ -35,20 +35,22 @@ describe("Thresholds", () => {
     expect(screen.queryByText(/Critical threshold:/)).not.toBeInTheDocument();
   });
 
-  // it("should render endpoint name and thresholds when data is present", () => {
-  //   render(
-  //     <Thresholds
-  //       endpointName="/api/test"
-  //       percentileValues={[1, 2, 3]}
-  //       thresholdsConfig={mockConfig}
-  //     />
-  //   );
-  //   expect(screen.getByText("/api/test")).toBeInTheDocument();
-  //   expect(screen.getByText(/Warning threshold:/)).toBeInTheDocument();
-  //   expect(screen.getByText(/10/)).toBeInTheDocument();
-  //   expect(screen.getByText(/Critical threshold:/)).toBeInTheDocument();
-  //   expect(screen.getByText(/20/)).toBeInTheDocument();
-  // });
+  it("should render endpoint name and thresholds when data is present", () => {
+    render(
+      <Thresholds
+        sortedPercentileValues={[1, 2, 3]}
+        endpointName={"/api/test"}
+        thresholdsConfig={mockConfig}
+      />
+    );
+    const section = screen.getByRole("region", { hidden: true });
+    expect(section).toHaveClass(/opacity-100/);
+    expect(screen.getByText("/api/test")).toBeInTheDocument();
+    expect(screen.getByText(/Warning threshold:/)).toBeInTheDocument();
+    expect(screen.getByText(/10/)).toBeInTheDocument();
+    expect(screen.getByText(/Critical threshold:/)).toBeInTheDocument();
+    expect(screen.getByText(/20/)).toBeInTheDocument();
+  });
 
   // it("should apply correct aria-expanded attribute", () => {
   //   render(
