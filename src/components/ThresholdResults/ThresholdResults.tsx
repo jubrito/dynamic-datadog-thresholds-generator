f5efbf643ea140b3fa4c4da4f6840ee607528b4e
jubrito <jujubrito@outlook.com>
2025-10-01 23:27:32 -0300

    add aria live

--- src/components/ThresholdResults/ThresholdResults.tsx
+++ src/components/ThresholdResults/ThresholdResults.tsx
@@ -9,22 +9,18 @@ export const ThresholdResults = ({
   warningThreshold,
   criticalThreshold,
 }: ThresholdProps) => {
+  if (warningThreshold == null && criticalThreshold == null) return null;
+
   return (
     <>
       <div className="flex md:gap-5 flex-col md:flex-row">
         {warningThreshold != null && (
-          <p
-            className="font-bold mt-6 text-lg p-2 bg-[#F66500] min-w-60 xl:w-1/6 w-full rounded-lg"
-            aria-live="polite"
-          >
+          <p className="font-bold mt-6 text-lg p-2 bg-[#F66500] min-w-60 xl:w-1/6 w-full rounded-lg">
             Warning threshold: {warningThreshold}
           </p>
         )}
         {criticalThreshold != null && (
-          <p
-            className="font-bold mt-6 p-2 pl-5 text-lg bg-pink-500 min-w-60 xl:w-1/6 w-full rounded-lg"
-            aria-live="polite"
-          >
+          <p className="font-bold mt-6 p-2 pl-5 text-lg bg-pink-500 min-w-60 xl:w-1/6 w-full rounded-lg">
             Critical threshold: {criticalThreshold}
           </p>
         )}
