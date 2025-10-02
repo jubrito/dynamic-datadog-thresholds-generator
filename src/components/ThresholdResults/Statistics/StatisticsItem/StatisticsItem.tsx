6bb2c395a9b61890747ab423997108d652c31f95
jubrito <jujubrito@outlook.com>
2025-10-01 23:07:02 -0300

    add StatisticsItem

---
+++ src/components/ThresholdResults/Statistics/StatisticsItem/StatisticsItem.tsx
@@ -0,0 +1,33 @@
+import { darkGrayBlue } from "../../../../utils/styles";
+
+type StatisticsItemProps = {
+  label?: string;
+  value?: string | number;
+  styles?: {
+    container?: string;
+    label?: string;
+    value?: string;
+  };
+};
+export const StatisticsItem = ({
+  label,
+  value,
+  styles = {
+    container: "",
+    label: "",
+    value: "",
+  },
+}: StatisticsItemProps) => {
+  return (
+    <p
+      className={`border-1 p-3 rounded-lg flex-grow-1 ${darkGrayBlue.border} ${styles.container}`}
+    >
+      <strong>
+        {label && <span className={styles?.label}>{label}</span>}
+        {value && (
+          <span className={`ml-2 text-cyan-500 ${styles.value}`}>{value}</span>
+        )}
+      </strong>
+    </p>
+  );
+};
