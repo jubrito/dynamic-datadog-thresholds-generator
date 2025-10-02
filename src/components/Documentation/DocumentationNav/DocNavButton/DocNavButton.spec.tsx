ea16154b4f71f8415d1efc984ef3b850c4df4168
jubrito <jujubrito@outlook.com>
2025-10-02 00:02:36 -0300

    test: should call onClick when button is clicked

--- src/components/Documentation/DocumentationNav/DocNavButton/DocNavButton.spec.tsx
+++ src/components/Documentation/DocumentationNav/DocNavButton/DocNavButton.spec.tsx
@@ -8,4 +8,13 @@ describe("DocNavButton", () => {
     render(<DocNavButton onClick={jest.fn()} label={label} isOpen={true} />);
     expect(screen.getByText(label)).toBeInTheDocument();
   });
+  it("should call onClick when button is clicked", async () => {
+    const onClickMock = jest.fn();
+    render(<DocNavButton onClick={onClickMock} label={label} isOpen={true} />);
+    const documentationButton = screen.getByRole("button", {
+      name: `${label} documentation page`,
+    });
+    await userEvent.click(documentationButton);
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  });
 });
