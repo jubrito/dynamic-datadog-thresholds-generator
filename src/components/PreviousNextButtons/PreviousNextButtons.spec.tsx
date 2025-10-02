ff01031eca33dd62d508d0617b658e34ab923b08
jubrito <jujubrito@outlook.com>
2025-10-01 23:46:05 -0300

    test: should render previous and next buttons when props are provided

---
+++ src/components/PreviousNextButtons/PreviousNextButtons.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { PreviousNextButtons } from "./PreviousNextButtons";
+
+describe("PreviousNextButtons", () => {
+  const nextActionMock = jest.fn();
+  const nextLabel = "Next";
+  const previousLabel = "Previous";
+  const previousActionMock = jest.fn();
+  it("should render previous and next buttons when props are provided", () => {
+    render(
+      <PreviousNextButtons
+        next={{ action: nextActionMock, label: nextLabel }}
+        previous={{ action: previousActionMock, label: previousLabel }}
+      />
+    );
+    expect(screen.getByText(nextLabel)).toBeInTheDocument();
+    expect(screen.getByText(previousLabel)).toBeInTheDocument();
+  });
+});
