e391c3129cf25ad0a7c29be9dc8c2729f819261d
jubrito <jujubrito@outlook.com>
2025-10-02 00:02:04 -0300

    add label

--- src/components/Documentation/DocumentationNav/DocNavButton/DocNavButton.tsx
+++ src/components/Documentation/DocumentationNav/DocNavButton/DocNavButton.tsx
@@ -3,6 +3,7 @@ type DocNavButtonProps = {
   isOpen: boolean;
   label: string;
 };
+
 export const DocNavButton = ({
   onClick,
   label,
@@ -14,6 +15,7 @@ export const DocNavButton = ({
       className={`${
         isOpen ? "font-bold border-b-1" : "text-[#dedae3] "
       } relative group text-lg hover:text-white cursor-pointer text-right`}
+      aria-label={`${label} documentation page`}
     >
       <span>{label}</span>
       <span className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-indigo-500 group-hover:w-full"></span>
