f099686161e634db81a7ec9bc19775591d0f3a7b
jubrito <jujubrito@outlook.com>
2025-10-01 23:33:11 -0300

    test: should add default styles

--- src/components/BorderBox/BorderBox.spec.tsx
+++ src/components/BorderBox/BorderBox.spec.tsx
@@ -6,6 +6,10 @@ describe("BorderBox", () => {
     const { getByText } = render(<BorderBox>Content</BorderBox>);
     expect(getByText("Content")).toBeInTheDocument();
   });
+  it("should add default styles", () => {
+    const { container } = render(<BorderBox>Content</BorderBox>);
+    expect(container.firstChild).toHaveClass("border-1 border-white p-5 w-max");
+  });
   it("should apply custom styles", () => {
     const { container } = render(
       <BorderBox style="custom-style">Content</BorderBox>
