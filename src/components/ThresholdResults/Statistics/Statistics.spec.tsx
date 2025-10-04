f778bb6c220c73156f805fc84654b29077691083
jubrito <jujubrito@outlook.com>
2025-10-04 00:39:07 -0300

    test: should show/hide stats section from screen readers based on the show stats state

--- src/components/ThresholdResults/Statistics/Statistics.spec.tsx
+++ src/components/ThresholdResults/Statistics/Statistics.spec.tsx
@@ -12,15 +12,6 @@ describe("EndpointStatistics", () => {
     sorted: "1,2,5,7,10",
   };
 
-  const statisticsItems = [
-    "Number of values analyzed:",
-    "Minimum:",
-    "Maximum:",
-    "Average:",
-    "Median:",
-    "Sorted percentiles:",
-  ];
-
   it("should visually hide statistics if show insights is false", () => {
     render(
       <EndpointStatistics endpointStats={baseStats} showInsights={false} />
@@ -72,6 +63,25 @@ describe("EndpointStatistics", () => {
     expect(statsSection).not.toHaveAttribute("aria-expanded", "true");
   });
 
+  it("should show/hide stats section from screen readers based on the show stats state", async () => {
+    render(
+      <EndpointStatistics endpointStats={baseStats} showInsights={true} />
+    );
+
+    expect(screen.getByLabelText("Endpoint insights")).toHaveAttribute(
+      "aria-hidden",
+      "true"
+    );
+
+    await userEvent.click(
+      screen.getByRole("button", { name: /Show endpoint insights/i })
+    );
+    expect(screen.getByLabelText("Endpoint insights")).toHaveAttribute(
+      "aria-hidden",
+      "false"
+    );
+  });
+
   it("should show insufficient data note if numberOfElements < 4", async () => {
     const notEnoughStats = {
       numberOfElements: 1,
@@ -107,18 +117,4 @@ describe("EndpointStatistics", () => {
 
     expect(btn).toHaveAttribute("aria-expanded", "true");
   });
-
-  it.each(statisticsItems)(
-    "should toggle each statistics item when pressing button",
-    async (statisticsItem) => {
-      render(
-        <EndpointStatistics endpointStats={baseStats} showInsights={true} />
-      );
-      await userEvent.click(screen.getByLabelText(/Show endpoint insights/));
-      expect(screen.queryByText(statisticsItem)).toBeInTheDocument();
-
-      await userEvent.click(screen.getByLabelText(/Hide endpoint insights/));
-      expect(screen.queryByText(statisticsItem)).not.toBeInTheDocument();
-    }
-  );
 });
