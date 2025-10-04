3b829e7decd1830e26580c26664280bc66e49227
jubrito <jujubrito@outlook.com>
2025-10-03 23:15:55 -0300

    test: should render range input with label and correct attributes

--- src/components/Configuration/ConfigOptions/RangeConfig/RangeConfig.spec.tsx
+++ src/components/Configuration/ConfigOptions/RangeConfig/RangeConfig.spec.tsx
@@ -43,20 +43,31 @@ describe("RangeConfig", () => {
       expect(numberInput).toHaveAttribute("max", limits.max.toString());
       expect(numberInput).toHaveAttribute("value", defaultValue.toString());
     });
-    it("should render low label hidden for screen readers", () => {
+    it("should render low label and hide it from screen readers", () => {
       const lowLabel = screen.getByText(labels.lowLabel);
       expect(lowLabel).toBeInTheDocument();
       expect(lowLabel).toHaveAttribute("aria-hidden", "true");
     });
-    it("should render high label", () => {
+    it("should render high label and hide it from screen readers", () => {
       const highLabel = screen.getByText(labels.highLabel);
       expect(highLabel).toBeInTheDocument();
       expect(highLabel).toHaveAttribute("aria-hidden", "true");
     });
-    it("should render range bar label with correct attributes", () => {
+    it("should render range bar label and hide it from screen readers", () => {
       const rangeBarLabel = screen.getByText(labels.rangeBarLabel);
       expect(rangeBarLabel).toBeInTheDocument();
       expect(rangeBarLabel).toHaveAttribute("aria-hidden", "true");
+    });
+    it("should render range input with label and correct attributes", () => {
+      const inputRangeLabel = `The ${
+        labels.field
+      } value represents the ${labels.rangeBarLabel.toLowerCase()}. A lower value means '${labels.lowLabel.toLowerCase()}' and a higher value means '${labels.highLabel.toLowerCase()}'`;
+      const rangeInput = screen.getByRole("slider", { name: inputRangeLabel });
+      expect(rangeInput).toBeInTheDocument();
+      expect(rangeInput).toHaveAttribute("type", "range");
+      expect(rangeInput).toHaveAttribute("min", limits.min.toString());
+      expect(rangeInput).toHaveAttribute("max", limits.max.toString());
+      expect(rangeInput).toHaveAttribute("value", defaultValue.toString());
     });
   });
 
