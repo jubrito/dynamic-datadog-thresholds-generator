dd38faeb17c028e5bb77bf030848675e932da434
jubrito <jujubrito@outlook.com>
2025-10-03 23:55:57 -0300

    test: should render range config wrapper

--- src/components/Configuration/ConfigOptions/ConfigOptions.spec.tsx
+++ src/components/Configuration/ConfigOptions/ConfigOptions.spec.tsx
@@ -24,6 +24,12 @@ describe("ConfigOptions", () => {
     expect(thresholdTypeField).toBeInTheDocument();
     expect(thresholdTypeField).toHaveAttribute("aria-hidden", "true");
   });
+  it("should render range config wrapper", () => {
+    const rangeConfigWrapper = screen.getByLabelText(
+      `${thresholdType} Threshold configuration`
+    );
+    expect(rangeConfigWrapper).toBeInTheDocument();
+  });
   //   it("should have a div with aria-labelledby attribute pointing to the threshold type field id", () => {
   //     const divElement = screen.getByRole("region", {
   //       name: `${thresholdType} Threshold`,
