2fd9dbb9ac9d1799acc0e779874b09130e45dac0
jubrito <jujubrito@outlook.com>
2025-10-04 00:47:32 -0300

    test: should not render when threshold data has no metric values

--- src/components/GeneratorResults/GeneratorResults.spec.tsx
+++ src/components/GeneratorResults/GeneratorResults.spec.tsx
@@ -12,48 +12,45 @@ describe("GeneratorResults", () => {
     critical: { percentile: 99, factor: 5 },
   };
   let originalScrollIntoView: typeof window.HTMLElement.prototype.scrollIntoView;
-  let rerenderGenerator: (ui: React.ReactElement) => void;
 
   beforeAll(() => {
     originalScrollIntoView = window.HTMLElement.prototype.scrollIntoView;
     window.HTMLElement.prototype.scrollIntoView = jest.fn();
   });
-
-  beforeEach(() => {
-    const { rerender } = render(
-      <GeneratorResults
-        thresholdData={thresholdData}
-        thresholdsConfig={thresholdsConfig}
-      />
-    );
-    rerenderGenerator = rerender;
-  });
-
   afterAll(() => {
     window.HTMLElement.prototype.scrollIntoView = originalScrollIntoView;
   });
 
-  it("should render endpoint path", () => {
-    const endpointNameField = screen.getByLabelText(
-      `Endpoint name: ${thresholdData.endpointPath}`
-    );
-    expect(endpointNameField).toBeInTheDocument();
-    expect(within(endpointNameField).getByText(/path/)).toBeInTheDocument();
-  });
-  it("should render thresholds suggestions", () => {
-    expect(screen.getByText(/Warning threshold: 3.9/)).toBeInTheDocument();
-    expect(screen.getByText(/Critical threshold: 7.98/)).toBeInTheDocument();
-  });
   it("should not render when threshold data has no metric values", () => {
-    rerenderGenerator(
+    const { container } = render(
       <GeneratorResults
         thresholdData={{ ...thresholdData, metricValues: [] }}
         thresholdsConfig={thresholdsConfig}
       />
     );
-    const endpointNameField = screen.queryByLabelText(
-      `Endpoint name: ${thresholdData.endpointPath}`
-    );
-    expect(endpointNameField).not.toBeInTheDocument();
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  describe("Default rendering", () => {
+    beforeEach(() => {
+      render(
+        <GeneratorResults
+          thresholdData={thresholdData}
+          thresholdsConfig={thresholdsConfig}
+        />
+      );
+    });
+
+    it("should render endpoint path", () => {
+      const endpointNameField = screen.getByLabelText(
+        `Endpoint name: ${thresholdData.endpointPath}`
+      );
+      expect(endpointNameField).toBeInTheDocument();
+      expect(within(endpointNameField).getByText(/path/)).toBeInTheDocument();
+    });
+    it("should render thresholds suggestions", () => {
+      expect(screen.getByText(/Warning threshold: 3.9/)).toBeInTheDocument();
+      expect(screen.getByText(/Critical threshold: 7.98/)).toBeInTheDocument();
+    });
   });
 });
