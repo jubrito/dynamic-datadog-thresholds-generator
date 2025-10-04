71248a0834f155a00ff60eab36194f88ef8a7643
jubrito <jujubrito@outlook.com>
2025-10-04 01:02:02 -0300

    test: should render section described by introduction description

---
+++ src/components/UploadCsvBox/UploadCsvBox.spec.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import { UploadCsvBox } from "./UploadCsvBox";
+
+jest.mock("filepond-plugin-image-exif-orientation", () => ({}));
+jest.mock("filepond-plugin-image-preview", () => ({}));
+jest.mock("filepond/dist/filepond.min.css", () => ({}));
+jest.mock(
+  "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css",
+  () => ({})
+);
+
+describe("UploadCsvBox", () => {
+  const updateThresholdDataMock = jest.fn();
+  beforeEach(() => {
+    render(<UploadCsvBox updateThresholdData={updateThresholdDataMock} />);
+  });
+
+  it("should render section described by introduction description", () => {
+    const section = screen.getByRole("region");
+    expect(section).toHaveAttribute(
+      "aria-describedby",
+      "extract-and-upload-csv-description"
+    );
+  });
+  // it('should render content', () => {
+  //     render(<UploadCsvBox updateThresholdData={updateThresholdDataMock} />);
+  //     const heading = screen.getByRole('heading', { name: /Upload your CSV file/i });
+  //     expect(heading).toBeInTheDocument();
+  //     const description = screen.getByText(extractAndUploadCsvDescriptionI);
+  //     expect(description).toBeInTheDocument();
+  //     const filePond = screen.getByRole('button', { name: /Add files/i });
+  //     expect(filePond).toBeInTheDocument();
+  // })
+});
