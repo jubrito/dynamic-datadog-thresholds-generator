6660e030d1dab34e0b84ba3c32039853042d34ce
jubrito <jujubrito@outlook.com>
2025-10-05 18:27:35 -0300

    test: should render observability section by default

--- src/features/Documentation/Documentation.spec.tsx
+++ src/features/Documentation/Documentation.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { Documentation } from "./Documentation";
 
 describe("Documentation", () => {
@@ -8,5 +8,12 @@ describe("Documentation", () => {
 
   it("should render documentation nav section", () => {
     expect(screen.getByRole("navigation")).toBeInTheDocument();
+  });
+
+  it("should render observability section by default", () => {
+    const section = screen.getByRole("region");
+    expect(
+      within(section).getByRole("heading", { level: 1, name: "Observability" })
+    ).toBeInTheDocument();
   });
 });
