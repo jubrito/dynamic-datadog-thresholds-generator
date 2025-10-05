41d7f0f0d1215e0b6529ecb470577754d497ebc1
jubrito <jujubrito@outlook.com>
2025-10-05 20:05:58 -0300

    test: should render all content

--- src/components/Documentation/DocumentationContent/Observability/Observability.spec.tsx
+++ src/components/Documentation/DocumentationContent/Observability/Observability.spec.tsx
@@ -6,24 +6,19 @@ import userEvent from "@testing-library/user-event";
 describe("Observability", () => {
   const openDocumentationMock = jest.fn();
   const page = "Observability";
-  const texts = [
+  const content = [
     "What is it and why does it matter?",
     "Observability is the practice of measuring the internal state of systems by examining outputs, such as metrics and logs.",
     "Observability tools are critical to detect, debug, and resolve performance issues before they impact users.",
     "To address observability, we can use tools such as Datadog, a plataform that provides an observability service to monitor servers, databases, tools, and services, through a SaaS-based data analytics platform.",
-  ];
-  const observabilityPractices = [
     "Observability practices can:",
-    "Increase system resilience",
-    "Provide faster feedback",
-  ];
-  const pillars = [
+    "Increase system resilience.",
+    "Provide faster feedback.",
     "The three pillars of observability are:",
-    "logs",
-    "metrics",
-    "traces",
+    "Logs",
+    "Metrics",
+    "Traces",
   ];
-  const content = [...texts, ...observabilityPractices, ...pillars];
 
   beforeEach(() => {
     render(<Observability openDocumentation={openDocumentationMock} />);
@@ -37,8 +32,14 @@ describe("Observability", () => {
     expect(title).toBeInTheDocument();
   });
 
-  //   it.each(content)("should render each content text", (contentText) => {
-  //     expect(screen.getByText(contentText)).toBeInTheDocument();
+  it.each(content)("should render all content", (contentText) => {
+    expect(screen.getByText(contentText)).toBeInTheDocument();
+  });
+
+  //   it.each(listItems)("should render each list item", (listItem) => {
+  //     expect(
+  //       screen.getByRole("listitem", { name: listItem })
+  //     ).toBeInTheDocument();
   //   });
 
   //   it('should render "Previous" button', () => {
