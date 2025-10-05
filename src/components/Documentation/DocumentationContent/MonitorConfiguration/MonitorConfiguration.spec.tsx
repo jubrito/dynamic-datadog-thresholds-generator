74e8f555f7b0bbd257e9040d771888b94d67f656
jubrito <jujubrito@outlook.com>
2025-10-05 19:56:05 -0300

    test: should not render "Next" button

--- src/components/Documentation/DocumentationContent/MonitorConfiguration/MonitorConfiguration.spec.tsx
+++ src/components/Documentation/DocumentationContent/MonitorConfiguration/MonitorConfiguration.spec.tsx
@@ -45,6 +45,14 @@ describe("MonitorConfiguration", () => {
     expect(previousButton).toBeInTheDocument();
   });
 
+  it('should not render "Next" button', () => {
+    const nextButton = screen.queryByRole("button", {
+      name: /Next page:/i,
+    });
+
+    expect(nextButton).not.toBeInTheDocument();
+  });
+
   it('should call function update page by clicking on "Previous" button', async () => {
     const previousButton = screen.getByRole("button", {
       name: /Previous page: Thresholds/,
