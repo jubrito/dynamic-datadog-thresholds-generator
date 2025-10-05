5738ce1d8969a0329690c582b963b93714a7c493
jubrito <jujubrito@outlook.com>
2025-10-05 19:20:59 -0300

    test: should render content

--- src/components/Documentation/DocumentationContent/Datadog/Datadog.spec.tsx
+++ src/components/Documentation/DocumentationContent/Datadog/Datadog.spec.tsx
@@ -17,4 +17,17 @@ describe("Datadog", () => {
     const title = screen.getAllByRole("heading", { level: 1, name: page });
     expect(title.length).toBeGreaterThan(0);
   });
+  it("should render content", () => {
+    const intro =
+      /is a leading full-stack observability platform that automates application performance monitoring, log management, infrastructure monitoring, and more. One of its most powerful features are/i;
+    const monitors =
+      /monitors with automated alerts that help teams detect when systems degrade or fail./i;
+    const question = /But how to define if a system is degrading or failing?/i;
+    const thresholds =
+      /When configuring monitors, you can define thresholds to create alerts and receive notifications about your endpoints./i;
+    expect(screen.getByText(intro)).toBeInTheDocument();
+    expect(screen.getByText(monitors)).toBeInTheDocument();
+    expect(screen.getByText(question)).toBeInTheDocument();
+    expect(screen.getByText(thresholds)).toBeInTheDocument();
+  });
 });
