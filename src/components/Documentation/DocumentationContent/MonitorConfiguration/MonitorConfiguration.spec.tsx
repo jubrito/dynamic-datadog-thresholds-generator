71debf4d5318d20d7bb8e24d3252bad672ff9023
jubrito <jujubrito@outlook.com>
2025-10-05 19:53:32 -0300

    test: should render title

---
+++ src/components/Documentation/DocumentationContent/MonitorConfiguration/MonitorConfiguration.spec.tsx
@@ -0,0 +1,35 @@
+import { screen } from "@testing-library/dom";
+import { MonitorConfiguration } from "./MonitorConfiguration";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("MonitorConfiguration", () => {
+  const openDocumentationMock = jest.fn();
+  const page = "Monitor Configuration";
+  const question = "How to configure and analyze Datadog monitors?";
+  const steps = [
+    "Define a query to extract metrics",
+    "Define a formula",
+    "Define the evaluation details",
+    "Define the thresholds",
+  ];
+  const examples = [
+    "Example: Retrieve the median of the request time in milliseconds for all endpoints X in production",
+    "Example: Convert the request time in milliseconds to request time seconds",
+    "Example: Evaluate the average of the query over the last 15 minutes",
+    "Example: Trigger alert when the evaluated value (from query) is above the alert threshold 6",
+  ];
+  const content = [question, ...steps, ...examples];
+
+  beforeEach(() => {
+    render(<MonitorConfiguration openDocumentation={openDocumentationMock} />);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render title", () => {
+    const title = screen.getByRole("heading", { level: 1, name: page });
+    expect(title).toBeInTheDocument();
+  });
+});
