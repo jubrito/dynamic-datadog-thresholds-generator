639f97fd189dc3dbd49b22aa055f74ede9924225
jubrito <jujubrito@outlook.com>
2025-10-05 19:08:46 -0300

    fix jest bug preventing assets imports

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,4 +8,8 @@ export default {
   extensionsToTreatAsEsm: [".ts", ".tsx"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
   testMatch: ["**/?(*.)+(test|spec).[tj]s?(x)"],
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$":
+      "<rootDir>/mocks/emptyFileMock.js",
+  },
 };
