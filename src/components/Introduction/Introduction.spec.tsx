160fd1a00b1e8e9351832d664b4ea868342b75c6
jubrito <jujubrito@outlook.com>
2025-10-14 23:27:42 -0300

    fix tests: use link with path instead of button with state

--- src/components/Introduction/Introduction.spec.tsx
+++ src/components/Introduction/Introduction.spec.tsx
@@ -1,13 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import { Introduction } from "./Introduction";
+import { BrowserRouter } from "react-router";
 
 describe("BorderBox", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <Introduction />
+      </BrowserRouter>
+    );
+  });
   it("should render title", () => {
-    render(<Introduction />);
     expect(screen.getByText("Dynamic Threshold Generator")).toBeInTheDocument();
   });
   it("should render description", () => {
-    render(<Introduction />);
     expect(
       screen.getByText(
         /Extract a Datadog endpoint metrics csv file using any percentile aggregation and upload it to generate warning and critical thresholds suggestions./
