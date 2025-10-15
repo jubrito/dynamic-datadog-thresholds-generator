f559483f3622ee931539b838aca472aea983e587
jubrito <jujubrito@outlook.com>
2025-10-14 23:22:07 -0300

    fix tests: use link with path instead of button with state

--- src/components/PreviousNextButtons/PreviousNextButtons.spec.tsx
+++ src/components/PreviousNextButtons/PreviousNextButtons.spec.tsx
@@ -1,50 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import { PreviousNextButtons } from "./PreviousNextButtons";
-import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router";
 
 describe("PreviousNextButtons", () => {
-  const nextActionMock = jest.fn();
   const nextLabel = "Next";
   const previousLabel = "Previous";
-  const previousActionMock = jest.fn();
-  it("should render previous and next buttons when props are provided", () => {
-    render(
-      <PreviousNextButtons
-        next={{ action: nextActionMock, label: nextLabel }}
-        previous={{ action: previousActionMock, label: previousLabel }}
-      />
-    );
-    expect(screen.getByText(`Next: ${nextLabel}`)).toBeInTheDocument();
-    expect(screen.getByText(`Previous: ${previousLabel}`)).toBeInTheDocument();
-  });
 
   it("should render previous and next buttons with aria label", async () => {
     render(
-      <PreviousNextButtons
-        next={{ action: nextActionMock, label: nextLabel }}
-        previous={{ action: previousActionMock, label: previousLabel }}
-      />
+      <BrowserRouter>
+        <PreviousNextButtons
+          next={{ path: "next", label: nextLabel }}
+          previous={{ path: "previous", label: previousLabel }}
+        />
+      </BrowserRouter>
     );
-
-    await userEvent.click(screen.getByText(`Next: ${nextLabel}`));
-    await userEvent.click(screen.getByText(`Previous: ${previousLabel}`));
-
     expect(
       screen.getByLabelText(`Next page: ${nextLabel}`)
     ).toBeInTheDocument();
     expect(
       screen.getByLabelText(`Previous page: ${previousLabel}`)
     ).toBeInTheDocument();
-  });
-
-  it("should call previous and next buttons when props are provided", () => {
-    render(
-      <PreviousNextButtons
-        next={{ action: nextActionMock, label: nextLabel }}
-        previous={{ action: previousActionMock, label: previousLabel }}
-      />
-    );
-    expect(nextActionMock).toHaveBeenCalledTimes(1);
-    expect(previousActionMock).toHaveBeenCalledTimes(1);
   });
 });
