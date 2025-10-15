0f0a71c125183533a05f0e131c74cf1655e8404e
jubrito <jujubrito@outlook.com>
2025-10-14 23:04:40 -0300

    add routes consts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,3 +8,10 @@ export const closeDocumentationSections: OpenDocumentationSection = {
 };
 
 export const insufficientDataValue = 5;
+
+// Documentation routes
+export const DOCUMENTATION_ROUTE = "/documentation";
+export const OBSERVABILITY_KEY = "observability";
+export const DATADOG_KEY = "datadog";
+export const THRESHOLDS_KEY = "thresholds";
+export const MONITOR_CONFIG_KEY = "monitor-configuration";
