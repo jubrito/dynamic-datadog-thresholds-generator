27f2c53743bf6b3499b2f7f0d51e15be4384d977
jubrito <jujubrito@outlook.com>
2025-10-21 21:43:05 -0300

    test: should call setDisplayTooltip with false to close tooltip when clicking on close

---
+++ src/components/MainTooltip/MainTooltip.spec.tsx
@@ -0,0 +1,22 @@
+import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+import { MainTooltip } from "./MainTooltip";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+
+describe("MainTooltip", () => {
+  const setDisplayTooltipMock = jest.fn();
+
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <MainTooltip setDisplayTooltip={setDisplayTooltipMock} />
+      </MemoryRouter>
+    );
+  });
+  it("should call setDisplayTooltip with false to close tooltip when clicking on close", async () => {
+    const closeLabel = "Close explanation and go back to generator";
+    await userEvent.click(screen.getByLabelText(closeLabel));
+    expect(setDisplayTooltipMock).toHaveBeenCalledWith(false);
+  });
+});
