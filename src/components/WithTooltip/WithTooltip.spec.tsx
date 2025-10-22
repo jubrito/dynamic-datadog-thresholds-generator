a8728aadc059a31a92fe5ae011081eb5bf04090b
jubrito <jujubrito@outlook.com>
2025-10-21 22:08:38 -0300

    test: should display and hide tooltip with displayTooltip value when clicking on trigger

---
+++ src/components/WithTooltip/WithTooltip.spec.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import { WithTooltip } from "./WithTooltip";
+import userEvent from "@testing-library/user-event";
+
+describe("WithTooltip", () => {
+  it("should display and hide tooltip with displayTooltip value when clicking on trigger", async () => {
+    render(
+      <WithTooltip
+        tooltipComponent={(_, displayTooltip) => (
+          <button>Tooltip Display tooltip: {String(displayTooltip)}</button>
+        )}
+        triggerComponent={(setDisplayTooltip) => (
+          <button onClick={() => setDisplayTooltip((prevState) => !prevState)}>
+            Trigger Display tooltip
+          </button>
+        )}
+      />
+    );
+    const tooltipDisplay = screen.queryByRole("button", {
+      name: "Tooltip Display tooltip: false",
+    });
+    const trigger = screen.getByRole("button", {
+      name: "Trigger Display tooltip",
+    });
+    expect(tooltipDisplay).not.toBeInTheDocument();
+
+    await userEvent.click(trigger);
+
+    const tooltipHide = screen.getByRole("button", {
+      name: "Tooltip Display tooltip: true",
+    });
+    expect(tooltipHide).toBeInTheDocument();
+  });
+});
