fe553783500d76aa2f6eb3d23b79ff558e52059b
jubrito <jujubrito@outlook.com>
2025-10-21 22:57:58 -0300

    add spacing

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,5 +1,6 @@
 export const mainHorizontalSpacing =
   "mx-auto grid 2xl:grid-cols-[300px_1fr_300px] xl:grid-cols-[250px_1fr_250px] lg:grid-cols-[196px_1fr_196px] grid-cols-[30px_1fr_30px]";
+
 export const mainTopSpacing = "mt-5 bg-";
 
 export const stylesMainBlue = {
