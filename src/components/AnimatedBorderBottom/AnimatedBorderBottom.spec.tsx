5b5024b0956ceb1aa722ef80d1294f5c93326823
jubrito <jujubrito@outlook.com>
2025-11-02 16:56:19 -0300

    test: should display children

--- src/components/AnimatedBorderBottom/AnimatedBorderBottom.spec.tsx
+++ src/components/AnimatedBorderBottom/AnimatedBorderBottom.spec.tsx
@@ -3,12 +3,19 @@ import { AnimatedBorderBottom } from "./AnimatedBorderBottom";
 import { BrowserRouter } from "react-router";
 
 describe("AnimatedBorderBottom", () => {
+  // it("should not display anything if link and children are not provided", () => {
+  //   // render(<AnimatedBorderBottom />)
+
+  //   expect();
+  // });
+  const ariaLabel = "aria label";
+  const label = "label";
+  const path = "/path";
+  const style = "style";
+  const child = "child";
+
   describe("WHEN link is provided but children is not", () => {
-    const ariaLabel = "aria label";
-    const label = "label";
-    const path = "/path";
-    const style = "style";
-    it("should display link", () => {
+    beforeEach(() => {
       render(
         <BrowserRouter>
           <AnimatedBorderBottom
@@ -17,11 +24,28 @@ describe("AnimatedBorderBottom", () => {
           />
         </BrowserRouter>
       );
+    });
+    it("should display link", () => {
+      render(<AnimatedBorderBottom />);
       const link = screen.getByRole("link", { name: ariaLabel });
       expect(link).toBeInTheDocument();
       expect(link).toHaveAttribute("href", path);
       expect(link).toHaveTextContent(label);
       expect(link).toHaveAttribute("class", `relative group ${style}`);
+    });
+  });
+  describe("WHEN children is provided but link is not", () => {
+    beforeEach(() => {
+      render(
+        <BrowserRouter>
+          <AnimatedBorderBottom children={<span>{child}</span>} />
+        </BrowserRouter>
+      );
+    });
+    it("should display children", () => {
+      const children = screen.getByText(child);
+      expect(children).toBeInTheDocument();
+      expect(children.parentElement).toHaveClass("relative group");
     });
   });
 });
