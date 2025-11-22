f4b243404d6d3a62ab9a59ea262d8ba4e77487f2
jubrito <jujubrito@outlook.com>
2025-11-22 19:11:12 -0300

    correctly focus on modal when it opens

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -25,10 +25,17 @@ export const Modal = ({
   variant = VARIANTS.INFO,
 }: ModalProps) => {
   const [displayModal, setDisplayModal] = React.useState(isOpen);
+  const closeButtonRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     setDisplayModal(isOpen);
   }, [isOpen]);
+
+  React.useEffect(() => {
+    if (displayModal && closeButtonRef.current) {
+      closeButtonRef.current.focus();
+    }
+  }, [displayModal]);
 
   if (!displayModal) return null;
   const styles = VARIANT_STYLES[variant];
@@ -50,6 +57,7 @@ export const Modal = ({
         <div
           tabIndex={0}
           className="flex min-h-full items-end justify-center sm:items-center"
+          ref={closeButtonRef}
         >
           <div className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-max sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95">
             <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-4 sm:pb-4">
