e7efba2cae590ec34c5ab2b24cc548ac72de9baf
jubrito <jujubrito@outlook.com>
2025-11-22 18:15:57 -0300

    render ExternalLink if it is an external link

--- src/components/Modal/ActionLinks/ActionLinks.tsx
+++ src/components/Modal/ActionLinks/ActionLinks.tsx
@@ -27,6 +27,31 @@ const LINK_TYPES = {
   SECONDARY: "secondary",
 } as const;
 
+const ExternalLink = ({
+  pathToNavigate,
+  label,
+  styles,
+  linkType,
+}: InternalLinkProps) => {
+  const primaryLinkStyle = `${styles.bg} ${styles.bgText} ${styles.bgHover} sm:ml-4 mb-2 sm:mb-0 `;
+  const secondaryLinkStyle = "bg-white/10 text-white hover:bg-white/20";
+  const linkStyle =
+    linkType == LINK_TYPES.PRIMARY ? primaryLinkStyle : secondaryLinkStyle;
+
+  if (!label) return null;
+
+  return (
+    <a
+      href={pathToNavigate}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`${linkStyle} cursor-pointer inline-flex w-full justify-center rounded-md leading-none px-3 py-2 text-sm font-bold sm:w-auto`}
+    >
+      {label}
+    </a>
+  );
+};
+
 const InternalLink = ({
   pathToNavigate,
   label,
@@ -57,14 +82,32 @@ export const ActionLinks = ({
 
   return (
     <div className="bg-gray-700/25 px-4 py-4 sm:flex sm:flex-row-reverse sm:px-6">
-      <InternalLink
-        styles={styles}
-        label={primaryLink.label}
-        pathToNavigate={primaryLink.pathToNavigate}
-        linkType={LINK_TYPES.PRIMARY}
-      />
-      {secondaryLink && (
+      {!primaryLink.externalLink && (
         <InternalLink
+          styles={styles}
+          label={primaryLink.label}
+          pathToNavigate={primaryLink.pathToNavigate}
+          linkType={LINK_TYPES.PRIMARY}
+        />
+      )}
+      {primaryLink.externalLink && (
+        <ExternalLink
+          styles={styles}
+          label={primaryLink.label}
+          pathToNavigate={primaryLink.pathToNavigate}
+          linkType={LINK_TYPES.PRIMARY}
+        />
+      )}
+      {secondaryLink && !secondaryLink.externalLink && (
+        <InternalLink
+          styles={styles}
+          label={secondaryLink.label}
+          pathToNavigate={secondaryLink.pathToNavigate}
+          linkType={LINK_TYPES.SECONDARY}
+        />
+      )}
+      {secondaryLink && secondaryLink.externalLink && (
+        <ExternalLink
           styles={styles}
           label={secondaryLink.label}
           pathToNavigate={secondaryLink.pathToNavigate}
