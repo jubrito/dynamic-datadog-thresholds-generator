046d0b68a11d3b3588498ffa8eff677c90174d8a
jubrito <jujubrito@outlook.com>
2025-11-22 18:12:11 -0300

    define VARIANT_STYLES

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,13 +1,40 @@
+import { VARIANTS } from "./constants";
+
 export const mainHorizontalSpacing =
   "mx-auto grid 2xl:grid-cols-[300px_1fr_300px] xl:grid-cols-[250px_1fr_250px] lg:grid-cols-[200px_1fr_200px] md:grid-cols-[150px_1fr_150px] grid-cols-[30px_1fr_30px]";
 
 export const mainTopSpacing = "mt-5 bg-";
+
+export const VARIANT_STYLES = {
+  [VARIANTS.ERROR]: {
+    bg: "bg-red-500",
+    shadow: "bg-red-500/10",
+    icon: "text-red-500",
+    bgText: "text-white",
+    bgHover: "hover:bg-red-600",
+  },
+  [VARIANTS.WARNING]: {
+    bg: "bg-orange-500",
+    shadow: "bg-orange-500/10",
+    icon: "text-orange-500",
+    bgText: "text-black",
+    bgHover: "hover:bg-orange-600",
+  },
+  [VARIANTS.INFO]: {
+    bg: "bg-cyan-500",
+    shadow: "bg-cyan-500/10",
+    icon: "text-cyan-500",
+    bgText: "text-black-500",
+    bgHover: "hover:bg-cyan-600",
+  },
+} as const;
 
 export const stylesMainBlue = {
   background: "bg-[#0b1220]",
   border: "border-[#0b1220]",
   shadow: "shadow-[#0b1220]",
 };
+
 export const stylesDarkGrayBlue = {
   background: "bg-[#051027ff]",
   border: "border-[#051027ff]",
