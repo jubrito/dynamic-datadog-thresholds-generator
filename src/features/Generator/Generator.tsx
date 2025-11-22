8bd9684b99eeb06321beb5c16b2adf70232a3d4f
jubrito <jujubrito@outlook.com>
2025-11-22 19:12:46 -0300

    add modal to generator

--- src/features/Generator/Generator.tsx
+++ src/features/Generator/Generator.tsx
@@ -7,6 +7,8 @@ import { DynamicVisibility } from "../../components/DynamicVisibility/DynamicVis
 import { GeneratorResults } from "./GeneratorResults/GeneratorResults";
 import { Configuration } from "./Configuration/Configuration";
 import { UploadCsvBox } from "./UploadCsvBox/UploadCsvBox";
+import { Modal } from "../../components/Modal/Modal";
+import { VARIANTS } from "../../utils/constants";
 
 export const Generator = () => {
   const [thresholdsConfig, setThresholdsConfig] = useState<ThresholdsConfig>({
@@ -22,50 +24,78 @@ export const Generator = () => {
   const [thresholdData, setThresholdData] = useState<ThresholdData>({
     metricValues: [],
   });
+  const [fileUploadFailed, setFileUploadFailed] = useState(false);
   const showInsights = thresholdData.metricValues.length > 0;
 
   return (
-    <section className={`py-10 ${mainHorizontalSpacing} ${mainTopSpacing}`}>
-      <div />
-      <div
-        className={`relative grid gap-7 max-w-253 ${
-          showInsights ? "grid-rows-2" : ""
-        }`}
+    <>
+      <Modal
+        isOpen={fileUploadFailed}
+        onClose={() => {}}
+        title="The file uploaded is not valid"
+        primaryLink={{
+          label: "Learn how to upload valid files",
+          pathToNavigate: "TODO",
+        }}
+        secondaryLink={{
+          label: "Go to DataDog docs",
+          pathToNavigate: "https://docs.datadoghq.com/",
+        }}
+        variant={VARIANTS.ERROR}
       >
-        <div className="xl:col-span-2">
-          <DynamicVisibility
-            triggerComponent={(
-              setDisplayContent,
-              _,
-              ariaControlsIds,
-              isExpanded
-            ) => (
-              <Introduction
-                setDisplayContent={setDisplayContent}
-                ariaControlsIds={ariaControlsIds}
-                isExpanded={isExpanded}
-              />
-            )}
-            contentComponent={(setDisplayContent) => (
-              <MainTooltip setDisplayContent={setDisplayContent} />
-            )}
-            toHideComponent={() => (
-              <div className="grid gap-11 8xl:grid-cols-[minmax(300px,600px)_minmax(auto,600px)] xl:grid-cols-[minmax(0px,600px)_minmax(0,600px)] grid-cols-1">
-                <UploadCsvBox updateThresholdData={setThresholdData} />
-                <Configuration
-                  thresholdsConfig={thresholdsConfig}
-                  updateThresholdsConfig={setThresholdsConfig}
-                />
-              </div>
-            )}
-          />
+        <div>
+          <p>Failed to retrieve values to calculate the thresholds.</p>
+          <p>
+            To generate dynamic thresholds, upload a valid CSV file with your
+            endpoint's percentile metrics data.
+          </p>
         </div>
-      </div>
-      <GeneratorResults
-        thresholdData={thresholdData}
-        thresholdsConfig={thresholdsConfig}
-      />
-      <div />
-    </section>
+      </Modal>
+      <section className={`py-10 ${mainHorizontalSpacing} ${mainTopSpacing}`}>
+        <div />
+        <div
+          className={`relative grid gap-7 max-w-253 ${
+            showInsights ? "grid-rows-2" : ""
+          }`}
+        >
+          <div className="xl:col-span-2">
+            <DynamicVisibility
+              triggerComponent={(
+                setDisplayContent,
+                _,
+                ariaControlsIds,
+                isExpanded
+              ) => (
+                <Introduction
+                  setDisplayContent={setDisplayContent}
+                  ariaControlsIds={ariaControlsIds}
+                  isExpanded={isExpanded}
+                />
+              )}
+              contentComponent={(setDisplayContent) => (
+                <MainTooltip setDisplayContent={setDisplayContent} />
+              )}
+              toHideComponent={() => (
+                <div className="grid gap-11 8xl:grid-cols-[minmax(300px,600px)_minmax(auto,600px)] xl:grid-cols-[minmax(0px,600px)_minmax(0,600px)] grid-cols-1">
+                  <UploadCsvBox
+                    updateThresholdData={setThresholdData}
+                    setFileUploadFailed={setFileUploadFailed}
+                  />
+                  <Configuration
+                    thresholdsConfig={thresholdsConfig}
+                    updateThresholdsConfig={setThresholdsConfig}
+                  />
+                </div>
+              )}
+            />
+          </div>
+        </div>
+        <GeneratorResults
+          thresholdData={thresholdData}
+          thresholdsConfig={thresholdsConfig}
+        />
+        <div />
+      </section>
+    </>
   );
 };
