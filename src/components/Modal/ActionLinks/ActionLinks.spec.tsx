5e1e8771b714df42df0e0b104b9ac91b7c670a46
jubrito <jujubrito@outlook.com>
2025-11-23 16:21:16 -0300

    test: should render internal secondary primary link with path

--- src/components/Modal/ActionLinks/ActionLinks.spec.tsx
+++ src/components/Modal/ActionLinks/ActionLinks.spec.tsx
@@ -5,22 +5,22 @@ import { BrowserRouter } from "react-router";
 describe("ActionLinks", () => {
   const internalPathToNavigate = "/path";
   const externalPathToNavigate = "www.external.com";
+  const primaryLabel = "primary-label";
+  const secondaryLabel = "secondary-label";
 
   describe("Primary Link", () => {
-    const label = "label";
-
     it("should render internal primary link with path", () => {
       render(
         <BrowserRouter>
           <ActionLinks
             primaryLink={{
               pathToNavigate: internalPathToNavigate,
-              label,
+              label: primaryLabel,
             }}
           />
         </BrowserRouter>
       );
-      const link = screen.getByRole("link", { name: label });
+      const link = screen.getByRole("link", { name: primaryLabel });
       expect(link).toBeInTheDocument();
       expect(link).toHaveAttribute("href", internalPathToNavigate);
     });
@@ -31,13 +31,35 @@ describe("ActionLinks", () => {
           <ActionLinks
             primaryLink={{
               pathToNavigate: internalPathToNavigate,
-              label,
+              label: primaryLabel,
               isExternalLink: true,
             }}
           />
         </BrowserRouter>
       );
-      const link = screen.getByRole("anchor", { name: label });
+      const link = screen.getByRole("anchor", { name: primaryLabel });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", internalPathToNavigate);
+    });
+  });
+
+  describe("Primary Link", () => {
+    it("should render internal secondary primary link with path", () => {
+      render(
+        <BrowserRouter>
+          <ActionLinks
+            primaryLink={{
+              pathToNavigate: internalPathToNavigate,
+              label: primaryLabel,
+            }}
+            secondaryLink={{
+              pathToNavigate: internalPathToNavigate,
+              label: secondaryLabel,
+            }}
+          />
+        </BrowserRouter>
+      );
+      const link = screen.getByRole("link", { name: secondaryLabel });
       expect(link).toBeInTheDocument();
       expect(link).toHaveAttribute("href", internalPathToNavigate);
     });
