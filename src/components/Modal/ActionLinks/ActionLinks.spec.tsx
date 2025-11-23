c53d95f3006b64647f31c7b088f07b26cb9eaed5
jubrito <jujubrito@outlook.com>
2025-11-23 16:23:04 -0300

    test: should render external secondary anchor correctly

--- src/components/Modal/ActionLinks/ActionLinks.spec.tsx
+++ src/components/Modal/ActionLinks/ActionLinks.spec.tsx
@@ -30,7 +30,7 @@ describe("ActionLinks", () => {
         <BrowserRouter>
           <ActionLinks
             primaryLink={{
-              pathToNavigate: internalPathToNavigate,
+              pathToNavigate: externalPathToNavigate,
               label: primaryLabel,
               isExternalLink: true,
             }}
@@ -39,7 +39,7 @@ describe("ActionLinks", () => {
       );
       const link = screen.getByRole("anchor", { name: primaryLabel });
       expect(link).toBeInTheDocument();
-      expect(link).toHaveAttribute("href", internalPathToNavigate);
+      expect(link).toHaveAttribute("href", externalPathToNavigate);
     });
   });
 
@@ -62,6 +62,27 @@ describe("ActionLinks", () => {
       const link = screen.getByRole("link", { name: secondaryLabel });
       expect(link).toBeInTheDocument();
       expect(link).toHaveAttribute("href", internalPathToNavigate);
+    });
+
+    it("should render external secondary anchor correctly", () => {
+      render(
+        <BrowserRouter>
+          <ActionLinks
+            primaryLink={{
+              pathToNavigate: internalPathToNavigate,
+              label: primaryLabel,
+            }}
+            secondaryLink={{
+              pathToNavigate: externalPathToNavigate,
+              label: secondaryLabel,
+              isExternalLink: true,
+            }}
+          />
+        </BrowserRouter>
+      );
+      const link = screen.getByRole("anchor", { name: secondaryLabel });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", externalPathToNavigate);
     });
   });
 });
