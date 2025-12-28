de1d4fb20ce3232602091a05f3c4f8f85e4ac579
jubrito <jujubrito@outlook.com>
2025-12-28 19:24:07 -0300

    test: should render title

---
+++ src/features/Documentation/DocumentationContent/MonitoringStrategies/MonitoringStrategies.spec.tsx
@@ -0,0 +1,42 @@
+import { screen } from "@testing-library/dom";
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router";
+import { MonitoringStrategies } from "./MonitoringStrategies";
+
+describe("Monitoring Strategies", () => {
+  const page = "Monitoring Strategies";
+
+  const blanketMonitorsContent = [
+    /Blanket Monitors/,
+    /Singular latency threshold/,
+    /Based on median \(p50\)/,
+    /Measures the typical user experience/,
+    /Simple setup, but less scalable/,
+    /Alerts might not reflect endpoints' reality: what's considered "normal" for one endpoint could be "abnormal" for another/,
+  ];
+  const targetedMonitorsContent = [
+    /Targeted Monitors/,
+    /Specific tailored thresholds per endpoint/,
+    /Can be based on specific percentiles \(ex: p95\)/,
+    /Measures how slow users are affected/,
+    /Laborious setup, but more scalable/,
+    /Meaningful alerts considering endpoints characteristics: each threshold defined considers what is "normal" for that endpoint/,
+  ];
+  const content = [...blanketMonitorsContent, ...targetedMonitorsContent];
+
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <MonitoringStrategies />
+      </BrowserRouter>
+    );
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render title", () => {
+    const title = screen.getByRole("heading", { level: 1, name: page });
+    expect(title).toBeInTheDocument();
+  });
+});
