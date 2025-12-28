18a57e6f6280c6278716f5fcac1aa949452f1813
jubrito <jujubrito@outlook.com>
2025-12-28 17:35:16 -0300

    feat: use correct react properties

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -70,15 +70,15 @@ export const Modal = ({
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       data-slot="icon"
                       aria-hidden="true"
                       className={`size-6 ${styles.icon}`}
                     >
                       <path
                         d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       />
                     </svg>
                   </div>
