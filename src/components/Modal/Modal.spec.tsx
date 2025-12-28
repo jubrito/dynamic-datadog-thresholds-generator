37a3f9da698606c89b1c929dad2e39df42408931
jubrito <jujubrito@outlook.com>
2025-12-28 17:30:15 -0300

    test: should render title

---
+++ src/components/Modal/Modal.spec.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { Modal } from "./Modal";
+import { VARIANTS } from "../../utils/constants";
+import { BrowserRouter } from "react-router";
+
+describe("Modal", () => {
+  describe("default rendering", () => {
+    const title = "title";
+    const content = "content";
+    const primaryLink = { label: "primary", pathToNavigate: "/primary-path" };
+    const secondaryLink = {
+      label: "secondary",
+      pathToNavigate: "/secondary-path",
+    };
+    const onCloseMock = jest.fn();
+
+    beforeEach(() => {
+      render(
+        <BrowserRouter>
+          <Modal
+            isOpen={true}
+            onClose={onCloseMock}
+            title={title}
+            primaryLink={primaryLink}
+            secondaryLink={secondaryLink}
+            variant={VARIANTS.WARNING}
+          >
+            {content}
+          </Modal>
+        </BrowserRouter>
+      );
+    });
+
+    it("should render title", () => {
+      expect(screen.getByText(title)).toBeInTheDocument();
+    });
+  });
+});
