58f64679c9b2950e33478abf5f747143745e95b1
jubrito <jujubrito@outlook.com>
2025-12-28 19:24:02 -0300

    feat: add content to Monitoring Strategies page

--- src/features/Documentation/DocumentationContent/MonitoringStrategies/MonitoringStrategies.tsx
+++ src/features/Documentation/DocumentationContent/MonitoringStrategies/MonitoringStrategies.tsx
@@ -1,17 +1,91 @@
+import { ColorBox } from "../../../../components/ColorBox/ColorBox";
 import { Divider } from "../../../../components/Divider/Divider";
 import { PreviousNextButtons } from "../../../../components/PreviousNextButtons/PreviousNextButtons";
-import { DATADOG_KEY, DOCUMENTATION_ROUTE } from "../../../../utils/constants";
+import {
+  DOCUMENTATION_ROUTE,
+  MONITOR_CONFIG_KEY,
+} from "../../../../utils/constants";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 export const MonitoringStrategies = () => {
   return (
     <div className="flex flex-col gap-5">
       <h1 className="text-4xl font-bold">Monitoring Strategies</h1>
-      <p className="text-2xl">Blanket Monitors</p>
+      <table className="column-gap-20">
+        <thead>
+          <tr>
+            <th className="text-2xl">
+              <ColorBox style="px-5 bg-pink-600 mr-10">
+                Blanket Monitors
+              </ColorBox>
+            </th>
+            <th className="text-2xl">
+              <ColorBox style="px-5 bg-violet-500">Targeted Monitors</ColorBox>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="pt-4 pr-10">
+              <PlayArrowIcon className="text-pink-600 pr-1 " />
+              <strong>Singular latency threshold</strong>
+            </td>
+            <td className="pt-4">
+              <PlayArrowIcon className="text-violet-500 pr-1" />
+              <strong>Specific tailored thresholds per endpoint</strong>
+            </td>
+          </tr>
+          <tr>
+            <td className="pt-4 pr-10">
+              <PlayArrowIcon className="text-pink-600 pr-1" />
+              Based on median (p50)
+            </td>
+            <td className="pt-4">
+              <PlayArrowIcon className="text-violet-500 pr-1" />
+              Can be based on specific percentiles (ex: p95)
+            </td>
+          </tr>
+          <tr>
+            <td className="pt-4 pr-10">
+              <PlayArrowIcon className="text-pink-600 pr-1" />
+              Measures the typical user experience
+            </td>
+            <td className="pt-4">
+              <PlayArrowIcon className="text-violet-500 pr-1" />
+              Measures how slow users are affected
+            </td>
+          </tr>
+          <tr>
+            <td className="pt-4 pr-10">
+              <PlayArrowIcon className="text-pink-600 pr-1" />
+              Simple setup, but less scalable
+            </td>
+            <td className="pt-4">
+              <PlayArrowIcon className="text-violet-500 pr-1" />
+              Laborious setup, but more scalable
+            </td>
+          </tr>
+          <tr>
+            <td className="pt-4 font-bold pr-10">
+              <p className="border-1 border-pink-500 p-5 pr-0">
+                Alerts might not reflect endpoints' reality: what's considered
+                "normal" for one endpoint could be "abnormal" for another
+              </p>
+            </td>
+            <td className="pt-4 font-bold">
+              <p className="border-1 border-violet-500 p-5 pr-0">
+                Meaningful alerts considering endpoints characteristics: each
+                threshold defined considers what is "normal" for that endpoint
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <Divider />
       <PreviousNextButtons
-        next={{
-          label: "Datadog",
-          path: `${DOCUMENTATION_ROUTE}/${DATADOG_KEY}`,
+        previous={{
+          label: "Monitor Configuration",
+          path: `${DOCUMENTATION_ROUTE}/${MONITOR_CONFIG_KEY}`,
         }}
       />
     </div>
